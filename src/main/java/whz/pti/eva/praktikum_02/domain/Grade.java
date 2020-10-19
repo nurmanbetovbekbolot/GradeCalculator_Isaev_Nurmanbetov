d19baeb65c97ede0bc2d0b51f227f5ab3b26b1d8
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-10-19 17:01:56 +0200

    add JavaDocs and check for mistakes

--- src/main/java/whz/pti/eva/praktikum_02/domain/Grade.java
+++ src/main/java/whz/pti/eva/praktikum_02/domain/Grade.java
@@ -4,8 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * The entity Grade.
+ * The class Grade has three attributes id, lecture and grade
+ * In database will be created corresponding table
+ *
+ * @author Isaev A. Nurmanbetov B.
+ */
 @Entity
 public class Grade {
+
     @Id
     @GeneratedValue
     private Integer id;
@@ -13,40 +21,88 @@ public class Grade {
     private String lecture;
     private String grade;
 
+    /**
+     * Instantiates a new Grade.
+     */
     public Grade() {
     }
 
+    /**
+     * Instantiates a new Grade.
+     *
+     * @param lecture the lecture
+     * @param grade   the grade
+     */
     public Grade(String lecture, String grade) {
         this.lecture = lecture;
         this.grade = grade;
     }
 
+    /**
+     * Instantiates a new Grade.
+     *
+     * @param id      the id
+     * @param lecture the lecture
+     * @param grade   the grade
+     */
     public Grade(Integer id, String lecture, String grade) {
         this.id = id;
         this.lecture = lecture;
         this.grade = grade;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+
+    /**
+     * Gets lecture.
+     *
+     * @return the lecture
+     */
     public String getLecture() {
         return lecture;
     }
 
+
+    /**
+     * Sets lecture.
+     *
+     * @param lecture the lecture
+     */
     public void setLecture(String lecture) {
         this.lecture = lecture;
     }
 
+    /**
+     * Gets grade.
+     *
+     * @return the grade
+     */
     public String getGrade() {
         return grade;
     }
 
+    /**
+     * Sets grade.
+     *
+     * @param grade the grade
+     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
--- src/main/java/whz/pti/eva/praktikum_02/service/GradeService.java
+++ src/main/java/whz/pti/eva/praktikum_02/service/GradeService.java
@@ -4,11 +4,34 @@ import whz.pti.eva.praktikum_02.domain.Grade;
 
 import java.util.List;
 
+/**
+ * The interface GradeService service.
+ * In this interface we define all methods
+ *
+ * @author Isaev A. Nurmanbetov B.
+ */
 public interface GradeService {
 
+    /**
+     * Method listAllGrades which returns list all grades.
+     *
+     * @return the list of type Grade
+     */
     List<Grade> listAllGrades();
 
-    void addGrade (String lecture, String grade);
+    /**
+     * Method addGrade for adding new grades
+     * Method takes to arguments
+     *
+     * @param lecture the lecture
+     * @param grade   the grade
+     */
+    void addGrade(String lecture, String grade);
 
+    /**
+     * Method calculateAverage for calculating averages of grades
+     *
+     * @return average number of grades in type of double
+     */
     double calculateAverage();
 }
