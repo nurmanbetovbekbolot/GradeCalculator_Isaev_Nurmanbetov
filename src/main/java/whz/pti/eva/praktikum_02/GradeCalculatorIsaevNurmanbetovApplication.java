8006d90dd8671bee26969c99217b37c248606052
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-10-19 16:14:29 +0200

    init commit

---
+++ src/main/java/whz/pti/eva/praktikum_02/GradeCalculatorIsaevNurmanbetovApplication.java
@@ -0,0 +1,13 @@
+package whz.pti.eva.praktikum_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCalculatorIsaevNurmanbetovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCalculatorIsaevNurmanbetovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/whz/pti/eva/praktikum_02/GradeCalculatorIsaevNurmanbetovApplicationTests.java
@@ -0,0 +1,13 @@
+package whz.pti.eva.praktikum_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCalculatorIsaevNurmanbetovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
